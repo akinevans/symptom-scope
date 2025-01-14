ebe20d37b9687c2bff5fb3f63b84f21b2e5e7375
Akin Evans <akinevans@gmail.com>
2025-01-13 23:35:30 -0800

    fix ellipses menu btn click trigger issue

--- src/components/SymptomCard.tsx
+++ src/components/SymptomCard.tsx
@@ -14,43 +14,55 @@ import { NavLink } from 'react-router';
 
 export default function SymptomCard(props) {
   return (
-    <NavLink
-      to='#'
-      onClick={() => {
-        alert('Symptom Card Clicked');
-      }}
-    >
-      <Card className='mb-2 max-w-[410px] '>
-        <CardHeader className='flex flex-row place-content-between align-top text-left'>
-          <div className=''>
-            <CardDescription className='mb-2'>{props.date}</CardDescription>
-            <CardTitle className='text-xl font-normal'>{props.title}</CardTitle>
-          </div>
-          {/* //& Severity badge and menu icon */}
-          <div className='mt-0 flex justify-center items-center h-fit w-fit'>
-            <Badge className={` ${props.severityColor} mr-2 font-medium `}>
-              {props.severityTitle}
-            </Badge>
+    <div className='relative'>
+      {/* //* placing the ellipsis outside of the NavLink allows for clicking on either the symptomCard OR the ellipses seperatley not both */}
+      <EllipsisVertical
+        color='#65768c'
+        className='absolute top-8 right-5 mr-[-15px] cursor-pointer'
+        onClick={() => {
+          alert('menu btn clicked');
+        }}
+      />
+      <NavLink
+        to='#'
+        onClick={() => {
+          alert('Symptom Card Clicked');
+        }}
+      >
+        <Card className='mb-2 max-w-[410px] '>
+          <CardHeader className='flex flex-row place-content-between align-top text-left'>
+            <div className=''>
+              <CardDescription className='mb-2'>{props.date}</CardDescription>
+              <CardTitle className='text-xl font-normal'>
+                {props.title}
+              </CardTitle>
+            </div>
+            {/* //& Severity badge and menu icon */}
+            <div className='mt-0 flex justify-center items-center h-fit w-fit'>
+              <Badge className={` ${props.severityColor} mr-2 font-medium `}>
+                {props.severityTitle}
+              </Badge>
 
-            <EllipsisVertical
+              {/* <EllipsisVertical
               color='#65768c'
               className='mr-[-15px]'
               onClick={() => {
                 alert('menu btn clicked');
               }}
-            />
+            /> */}
+            </div>
+          </CardHeader>
+          <div className='mt-[-5px] mb-4 flex justify-center'>
+            <Separator className='w-[70%] flex flex-row justify-center' />
           </div>
-        </CardHeader>
-        <div className='mt-[-5px] mb-4 flex justify-center'>
-          <Separator className='w-[70%] flex flex-row justify-center' />
-        </div>
-        <CardContent>
-          <p className='text-left'>
-            Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do
-            eiusmod tempor incididunt ut.
-          </p>
-        </CardContent>
-      </Card>
-    </NavLink>
+          <CardContent>
+            <p className='text-left'>
+              Lorem ipsum dolor sit amet, consectetur adipi scing elit, sed do
+              eiusmod tempor incididunt ut.
+            </p>
+          </CardContent>
+        </Card>
+      </NavLink>
+    </div>
   );
 }
